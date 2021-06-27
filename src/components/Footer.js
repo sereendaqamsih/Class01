298633a3653f1c0233ca6f168dbfbf10b71b9c5c
sereendaqamsih <sereen.aldaqamsih@gmail.com>
2021-06-27 18:28:53 +0300

    lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render (){
+        return (
+            <h6> the author: Sereen Aldaqamsih</h6>
+        )
+    }
+}
+
+export default Footer;
